ff2c14283432fc3d88b73c365b4e654d2c5cd0ec
Lorenzo Uriel <lorenzouriel394@gmail.com>
2024-12-17 16:19:19 -0300

    Monitoring server files started and check log files

---
+++ tasks-automation/filter-by-request.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd /home/waves/projects/shell-scripts/tasks-automation/files 
+
+if [ -z $1 ]
+then
+    while [ -z $requisicao ]
+    do
+        read -p "Voce esqueceu de colocar o parametro (GET,PUT,POST,DELETE): " requisicao
+        letra_maiuscula=$(echo $requisicao | awk '{ print toupper($1) }')
+    done
+else
+    letra_maiuscula=$(echo $1 | awk '{ print toupper($1) }')
+fi
+
+case $letra_maiuscula in
+        GET)
+        cat apache.log | grep GET
+        ;;
+
+        POST)
+        cat apache.log | grep POST
+        ;;
+
+        PUT)
+        cat apache.log | grep PUT
+        ;;
+
+        DELETE)
+        cat apache.log | grep DELETE
+        ;;
+
+        *)
+        echo "O parametro passado nao e valido"
+        ;;
+esac
---
+++ tasks-automation/filter-log-results.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /home/waves/projects/shell-scripts
+
+regex="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
+
+if [[ $1 =~ $regex ]]
+then
+    cat apache.log | grep $1
+    if [ $? -ne 0 ]
+    then 
+	    echo "O endereço IP não está presente no arquivo"
+    fi
+else
+    echo "Formato não é válido"
+fi
+
